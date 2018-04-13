329b940d7184b95d85aa4afcf10463fd6a54236f
2SilentJay <2silentjay@gmail.com>
2018-04-14 00:44:22 +0300

    the interface of binio::RawBuffer has been changed

--- src/binio/RawBuffer.h
+++ src/binio/RawBuffer.h
@@ -6,96 +6,85 @@
 namespace binio {
 
 /**
- *   begin_ptr
- *       | <- offset ->  | <- left ->|
- *       |B|B|B|B|B|B|B|B|B|B|B|B|B|B|
- *                       |
- *                   buffer_ptr
+ *
+ *   | <- offset ->  | <- left ->|
+ *   |B|B|B|B|B|B|B|B|B|B|B|B|B|B|
+ *                 |
+ *             assign point
  */
 
-using RawBufferConstDataType_t = const uint8_t;
-using RawBufferDataType_t = uint8_t;
-
-template <typename Type>
+template <typename RawPtr>
 class Buffer {
-	template <typename T>
-	friend Buffer<RawBufferDataType_t> make_buffer(T buf, size_t bytes);
+	friend class RawBuffer;
+	friend class ConstRawBuffer;
 
-	template <typename T>
-	friend Buffer<RawBufferConstDataType_t> make_const_buffer(T buf, size_t bytes);
-
-	Type* buffer_ptr;
-	Type* begin_ptr;
+protected:
+	RawPtr* buffer_ptr;
 	size_t left_bytes;
+	size_t size_bytes;
 	bool in_bounds;
 
-	Buffer(Type* buf, size_t len) noexcept :
+	Buffer() noexcept:
+	buffer_ptr(nullptr),
+	left_bytes(0),
+	size_bytes(0),
+	in_bounds(true) { }
+
+	Buffer(RawPtr* buf, size_t len) noexcept:
 	buffer_ptr(buf),
-	begin_ptr(buf),
 	left_bytes(len),
+	size_bytes(len),
 	in_bounds(true) { }
 
 public:
 
-	Buffer() noexcept :
-	buffer_ptr(nullptr),
-	begin_ptr(nullptr),
-	left_bytes(0),
-	in_bounds(false) { }
-
-	template<typename T>
-	Buffer(const Buffer<T>& buf) noexcept :
-	buffer_ptr(buf.get()),
-	begin_ptr(buf.begin()),
-	left_bytes(buf.left()),
-	in_bounds(buf.bounds()) { }
-
-	template<typename T>
-	Buffer& operator=(const Buffer<T>& buf) noexcept {
-		buffer_ptr = buf.get();
-		begin_ptr = buf.begin();
-		left_bytes = buf.left();
-		in_bounds = buf.bounds();
-		return *this;
-	}
-
-	inline Type* get() const noexcept {
-		return buffer_ptr;
-	}
-
-	inline const Type* begin() const noexcept {
-		return begin_ptr;
-	}
-
 	inline size_t offset() const noexcept {
-		return buffer_ptr - begin_ptr;
+		return size_bytes - left_bytes;
 	}
 
 	inline size_t left() const noexcept {
 		return left_bytes;
 	}
 
-	/**
-	 * Check if the buffer is in bounds.
-	 * @return
-	 */
 	inline bool bounds() const noexcept {
 		return in_bounds;
 	}
 
 	/**
-	 * Return the buffer to the start state.
+	 * Set the assign point to the beginning of the buffer.
+	 * @param bytes - bytes to move
+	 * @return true - if the buffer is in bounds after moving
 	 */
-	inline void reset() noexcept {
-		left_bytes += offset();
-		buffer_ptr = begin_ptr;
-		in_bounds = true;
+	inline bool rewind() noexcept {
+		if (in_bounds) {
+			size_t offset = size_bytes - left_bytes;
+			buffer_ptr -= offset;
+			left_bytes += offset;
+		}
+		return in_bounds;
 	}
 
 	/**
-	 * Skip 'bytes' bytes and set 'buffer' at the new position.
-	 * @param bytes - bytes to skip
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during skipping
+	 * Set the assign point to a new position.
+	 * @param bytes - bytes to move
+	 * @return true - if the buffer is in bounds after moving
+	 */
+	inline bool rewind(size_t bytes) noexcept {
+		if (in_bounds) {
+			if (bytes > offset()) {
+				in_bounds = false;
+			} else {
+				buffer_ptr -= bytes;
+				left_bytes += bytes;
+			}
+		}
+		return in_bounds;
+	}
+
+	/**
+	 * Set the assign point to a new position.
+	 * @param bytes - bytes to move
+	 * @return true - if the buffer is in bounds after moving
 	 */
 	inline bool skip(size_t bytes) noexcept {
 		if (in_bounds) {
@@ -113,7 +102,7 @@ public:
 	 * Read the variables from the buffer and set it at the new position.
 	 * @param value - variable to read to
 	 * @param args - variables to read to
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during reading
+	 * @return true - if the buffer is in bounds after reading
 	 */
 	template <typename T, typename... Args>
 	inline bool read(T& value, Args&... args) noexcept {
@@ -123,7 +112,7 @@ public:
 	/**
 	 * Read one variable from the buffer and set it at the new position.
 	 * @param value - variable to read to
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during reading
+	 * @return true - if the buffer is in bounds after reading
 	 */
 	template <typename T>
 	inline bool read(T& value) noexcept {
@@ -140,9 +129,80 @@ public:
 	}
 
 	/**
+	 * Read an array from the buffer and set it at the new position.
+	 * @param value - variable to read to
+	 * @return true - if the buffer is in bounds after reading
+	 */
+	template <typename T>
+	inline bool read(T* array, const size_t array_len) noexcept {
+		if (in_bounds) {
+			size_t array_nb = array_len * sizeof (T);
+			if (array_nb > left_bytes) {
+				in_bounds = false;
+			} else {
+				for (size_t i = 0; i < array_len; i++) {
+					array[i] = *reinterpret_cast<const T*>(buffer_ptr);
+					buffer_ptr += sizeof (T);
+				}
+				left_bytes -= array_nb;
+			}
+		}
+		return in_bounds;
+	}
+
+	/**
+	 * Assign the pointers to the buffer and set it at the new position.
+	 * @param value - variable to assign to
+	 * @param args - variables to assign to
+	 * @return true - if the buffer is in bounds after assigning
+	 */
+	template <typename T, typename... Args>
+	inline bool assign(T*& value, Args*&... args) noexcept {
+		return assign(value) && assign(args...);
+	}
+
+	/**
+	 * Assign one pointer to the buffer and set it at the new position.
+	 * @param value - variable to assign to
+	 * @param args - variables to assign to
+	 * @return true - if the buffer is in bounds after assigning
+	 */
+	template <typename T>
+	inline bool assign(T*& value) noexcept {
+		if (in_bounds) {
+			if (sizeof (T) > left_bytes) {
+				in_bounds = false;
+			} else {
+				value = reinterpret_cast<T*>(buffer_ptr);
+				buffer_ptr += sizeof (T);
+				left_bytes -= sizeof (T);
+			}
+		}
+		return in_bounds;
+	}
+};
+
+using RawType_t = uint8_t;
+using ConstRawType_t = const uint8_t;
+
+class RawBuffer: public Buffer<RawType_t> {
+	using Base = Buffer<RawType_t>;
+
+public:
+
+	RawBuffer() noexcept: Base() { }
+
+	template <typename T>
+	RawBuffer(T* buf, size_t buf_len) noexcept:
+	Base(reinterpret_cast<RawType_t*>(buf), buf_len * sizeof (T)) { }
+
+	//	RawBuffer as_region() noexcept {
+	//	}
+
+	/**
 	 * Write the variables to the buffer and set it at the new position.
 	 * @param value - variable to write from
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during writing
+	 * @return true - if the buffer is in bounds after writing
 	 */
 	template <typename T, typename... Args>
 	inline bool write(const T& value, const Args&... args) noexcept {
@@ -153,7 +213,7 @@ public:
 	 * Write one variable to the buffer and set it at the new position.
 	 * @param value - variable to write from
 	 * @param args - variables to write from
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during writing
+	 * @return true - if the buffer is in bounds after writing
 	 */
 	template <typename T>
 	inline bool write(const T& value) noexcept {
@@ -170,49 +230,48 @@ public:
 	}
 
 	/**
-	 * Assign the pointers to the buffer and set it at the new position.
-	 * @param value - variable to assign to
-	 * @param args - variables to assign to
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during writing
-	 */
-	template <typename T, typename... Args>
-	inline bool assign(T*& value, Args*&... args) noexcept {
-		return assign(value) && assign(args...);
-	}
-
-	/**
-	 * Assign one pointer to the buffer and set it at the new position.
-	 * @param value - variable to assign to
-	 * @param args - variables to assign to
-	 * @return true - if the buffer is in bounds, false - if the buffer has been smashed during writing
+	 * Write an array to the buffer and set it at the new position.
+	 * @param value - variable to write from
+	 * @param args - variables to write from
+	 * @return true - if the buffer is in bounds after writing
 	 */
 	template <typename T>
-	inline bool assign(T*& value) {
+	inline bool write(const T* array, const size_t array_len) noexcept {
 		if (in_bounds) {
-			if (sizeof (T) > left_bytes) {
+			size_t array_nb = array_len * sizeof (T);
+			if (array_nb > left_bytes) {
 				in_bounds = false;
 			} else {
-				value = reinterpret_cast<T*>(buffer_ptr);
-				buffer_ptr += sizeof (T);
-				left_bytes -= sizeof (T);
+				for (size_t i = 0; i < array_len; i++) {
+					*reinterpret_cast<const T*>(buffer_ptr) = array[i];
+					buffer_ptr += sizeof (T);
+				}
+				left_bytes -= array_nb;
 			}
 		}
 		return in_bounds;
 	}
 };
 
-using ConstRawBuffer = Buffer<RawBufferConstDataType_t>;
-using RawBuffer = Buffer<RawBufferDataType_t>;
+class ConstRawBuffer: public Buffer<ConstRawType_t> {
+	using Base = Buffer<ConstRawType_t>;
 
-template <typename T>
-inline ConstRawBuffer make_const_buffer(T buf, size_t bytes) {
-	return ConstRawBuffer(reinterpret_cast<RawBufferConstDataType_t*>(buf), bytes);
-}
+public:
 
-template <typename T>
-inline RawBuffer make_buffer(T buf, size_t bytes) {
-	return RawBuffer(reinterpret_cast<RawBufferDataType_t*>(buf), bytes);
-}
+	ConstRawBuffer() noexcept: Base() { }
+
+	ConstRawBuffer(const RawBuffer& raw) noexcept: Base() {
+		Base::buffer_ptr = raw.buffer_ptr;
+		Base::left_bytes = raw.left_bytes;
+		Base::size_bytes = raw.size_bytes;
+		Base::in_bounds = raw.in_bounds;
+	}
+
+	template <typename T>
+	ConstRawBuffer(T* buf, size_t buf_len) noexcept:
+	Base(reinterpret_cast<ConstRawType_t*>(buf), buf_len * sizeof (T)) { }
+
+};
 
 
 }; // namespace binio
--- src/binio/RawBuffersTest.h
+++ src/binio/RawBuffersTest.h
@@ -24,81 +24,79 @@ class RawBuffersTest {
 		}
 	};
 
-	static void test_struct_rw(DataSet& set) {
+	static void test_read_write(DataSet& set) {
 		DataSet copy;
 
-		char raw_buffer[sizeof (DataSet)];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet));
+		unsigned raw_buffer_size = sizeof (DataSet) * 2;
+		char raw_buffer[raw_buffer_size];
+		RawBuffer buffer(raw_buffer, raw_buffer_size);
 
+		// writing
 		assert(buffer.bounds());
 		assert(buffer.write(set));
-		assert(buffer.bounds());
-		buffer.reset();
-		assert(buffer.bounds());
-		assert(buffer.read(copy));
-		assert(buffer.bounds());
-		assert(copy == set);
-	}
-
-	static void test_struct_assign(DataSet& set) {
-		DataSet* set_ptr;
-
-		char raw_buffer[sizeof (DataSet)];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet));
-
-		assert(buffer.bounds());
-		assert(buffer.write(set));
-		assert(buffer.bounds());
-		buffer.reset();
-		assert(buffer.bounds());
-		assert(buffer.assign(set_ptr));
-		assert(buffer.bounds());
-		assert(set == *set_ptr);
-	}
-
-	static void test_fields_rw(DataSet& set) {
-		DataSet copy;
-
-		char raw_buffer[sizeof (DataSet)];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet));
-
-		assert(buffer.bounds());
 		assert(buffer.write(set.c, set.s, set.i, set.l));
 		assert(buffer.bounds());
-		buffer.reset();
+		assert(buffer.rewind());
+
+		// reading
 		assert(buffer.bounds());
-		assert(buffer.read(copy.c, copy.s, copy.i, copy.l));
-		assert(buffer.bounds());
+		assert(buffer.read(copy));
 		assert(copy == set);
+
+		assert(buffer.read(copy.c, copy.s, copy.i, copy.l));
+		assert(copy == set);
+		assert(buffer.bounds());
 	}
 
-	static void test_fields_assign(DataSet& set) {
-		DataSet copy;
+	static void test_assign(DataSet& set) {
+		DataSet* copy;
 		const char* c_cptr = nullptr;
 		const short* s_cptr = nullptr;
 		int* i_ptr = nullptr;
 		long* l_ptr = nullptr;
 
-		char raw_buffer[sizeof (DataSet)];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet));
+		unsigned raw_buffer_size = sizeof (DataSet) * 2;
+		char raw_buffer[raw_buffer_size];
+		RawBuffer buffer(raw_buffer, raw_buffer_size);
 
+		// writing
 		assert(buffer.bounds());
+		assert(buffer.write(set));
 		assert(buffer.write(set.c, set.s, set.i, set.l));
 		assert(buffer.bounds());
-		buffer.reset();
+		assert(buffer.rewind());
+
+		// reading
 		assert(buffer.bounds());
+		assert(buffer.assign(copy));
+		assert(*copy == set);
+
 		assert(buffer.assign(c_cptr, s_cptr, i_ptr, l_ptr));
-
-		copy.c = *c_cptr;
-		copy.s = *s_cptr;
-		copy.i = *i_ptr;
-		copy.l = *l_ptr;
-
 		assert(buffer.bounds());
-		assert(copy == set);
+		assert(*c_cptr == set.c);
+		assert(*s_cptr == set.s);
+		assert(*i_ptr == set.i);
+		assert(*l_ptr == set.l);
 	}
 
-	static void test_smash() {
+	static void test_read_write_assign() {
+		DataSet input_false(0x00, 0x00, 0x00, 0x00);
+		DataSet input_true(~0, ~0, ~0, ~0);
+		DataSet input_inc(0x01, 0x02, 0x03, 0x04);
+		DataSet input_dec(~0 - 1, ~0 - 2, ~0 - 3, ~0 - 4);
+
+		test_read_write(input_false);
+		test_read_write(input_true);
+		test_read_write(input_inc);
+		test_read_write(input_dec);
+
+		test_assign(input_false);
+		test_assign(input_true);
+		test_assign(input_inc);
+		test_assign(input_dec);
+	}
+
+	static void test_bounds() {
 		char raw_buffer[2];
 		char w1 = 1;
 		char w2 = 128;
@@ -108,7 +106,7 @@ class RawBuffersTest {
 		char r2 = 0;
 		char r3 = 0;
 
-		RawBuffer buffer = make_buffer(raw_buffer, 2);
+		RawBuffer buffer(raw_buffer, 2);
 
 		assert(buffer.bounds());
 		assert(buffer.write(w1));
@@ -117,33 +115,34 @@ class RawBuffersTest {
 		assert(not buffer.write(w3));
 		assert(not buffer.bounds());
 
-		buffer.reset();
-		assert(buffer.bounds());
-		assert(buffer.write(w1, w2));
-		assert(buffer.bounds());
-		assert(not buffer.write(w3));
-		assert(not buffer.bounds());
+		RawBuffer buffer2(raw_buffer, 2);
+		assert(buffer2.bounds());
+		assert(buffer2.write(w1, w2));
+		assert(buffer2.bounds());
+		assert(not buffer2.write(w3));
+		assert(not buffer2.bounds());
 
-		buffer.reset();
-		assert(buffer.read(r1));
-		assert(buffer.read(r2));
-		assert(buffer.bounds());
-		assert(not buffer.read(r3));
-		assert(not buffer.bounds());
+		ConstRawBuffer buffer3(raw_buffer, 2);
+		assert(buffer3.read(r1));
+		assert(buffer3.read(r2));
+		assert(buffer3.bounds());
+		assert(not buffer3.read(r3));
+		assert(not buffer3.bounds());
 		assert(w1 == r1);
 		assert(w2 == r2);
 
-		buffer.reset();
-		assert(buffer.read(r1, r2));
-		assert(buffer.bounds());
-		assert(not buffer.read(r3));
-		assert(not buffer.bounds());
+		ConstRawBuffer buffer4(raw_buffer, 2);
+		assert(buffer4.read(r1, r2));
+		assert(buffer4.bounds());
+		assert(not buffer4.read(r3));
+		assert(not buffer4.bounds());
 		assert(w1 == r1);
 		assert(w2 == r2);
 	}
 
-	static void test_skip() {
-		unsigned char raw_buffer[2];
+	static void test_skip_rewind() {
+		unsigned raw_buffer_size = 3;
+		unsigned char raw_buffer[raw_buffer_size];
 		char w1 = 1;
 		char w2 = 128;
 		char w3 = 255;
@@ -152,142 +151,91 @@ class RawBuffersTest {
 		char r2 = 0;
 		char r3 = 0;
 
-		RawBuffer buffer = make_buffer(raw_buffer, 2);
+		RawBuffer buffer(raw_buffer, raw_buffer_size);
 		assert(buffer.write(w1));
 		assert(buffer.skip(1));
+		assert(buffer.rewind(2));
+		assert(buffer.skip(2));
+		assert(buffer.write(w3));
+		assert(buffer.rewind());
 		assert(buffer.bounds());
-		assert(not buffer.write(w3));
-		assert(not buffer.bounds());
 
-		buffer.reset();
 		assert(buffer.skip(1));
 		assert(buffer.write(w2));
+		assert(buffer.rewind(2));
+		assert(buffer.skip(2));
+		assert(buffer.write(w3));
+		assert(buffer.rewind());
 		assert(buffer.bounds());
-		assert(not buffer.write(w3));
-		assert(not buffer.bounds());
 
-		buffer.reset();
 		assert(buffer.read(r1));
 		assert(buffer.skip(1));
+		assert(buffer.rewind(2));
+		assert(buffer.skip(2));
+		assert(buffer.read(r3));
+		assert(buffer.rewind());
 		assert(buffer.bounds());
-		assert(not buffer.read(r3));
-		assert(not buffer.bounds());
 
-		buffer.reset();
+		assert(buffer.skip(1));
+		assert(buffer.rewind(1));
 		assert(buffer.skip(1));
 		assert(buffer.read(r2));
+		assert(buffer.skip(1));
+
 		assert(buffer.bounds());
-		assert(not buffer.read(r3));
-		assert(not buffer.bounds());
 
 		assert(w1 == r1);
 		assert(w2 == r2);
+		assert(w3 == r3);
 	}
 
-	static void test_raii_copy_ctr() {
+	static void test_raii() {
 		DataSet set_first(~0 - 1, ~0 - 2, ~0 - 3, ~0 - 4);
 		DataSet set_second(1, 2, 3, 4);
 
 		DataSet copy;
 
-		char raw_buffer[sizeof (DataSet) * 2];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet) * 2);
+		unsigned raw_buffer_size = sizeof (DataSet) * 2;
+		unsigned char raw_buffer[raw_buffer_size];
 
-		assert(buffer.bounds());
+		RawBuffer buffer(raw_buffer, raw_buffer_size);
 		assert(buffer.write(set_first));
 		assert(buffer.bounds());
 
 		RawBuffer buffer2(buffer);
-		assert(buffer.bounds());
 		assert(buffer.write(set_second));
 		assert(buffer.bounds());
 
 		ConstRawBuffer const_buffer(buffer2);
-		const_buffer.reset();
+		assert(const_buffer.rewind());
 		assert(const_buffer.bounds());
 		assert(const_buffer.read(copy));
-		assert(const_buffer.bounds());
 		assert(copy == set_first);
-
-		ConstRawBuffer const_buffer2(const_buffer);
-		assert(const_buffer2.bounds());
-		assert(const_buffer2.read(copy));
-		assert(const_buffer2.bounds());
+		assert(const_buffer.read(copy));
 		assert(copy == set_second);
-	}
-
-	static void test_raii_copy_opt() {
-		DataSet set_first(~0 - 1, ~0 - 2, ~0 - 3, ~0 - 4);
-		DataSet set_second(1, 2, 3, 4);
-
-		DataSet copy;
-
-		char raw_buffer[sizeof (DataSet) * 2];
-		RawBuffer buffer = make_buffer(raw_buffer, sizeof (DataSet) * 2);
-
-		assert(buffer.bounds());
-		assert(buffer.write(set_first));
-		assert(buffer.bounds());
-
-		RawBuffer buffer2;
-		buffer2 = buffer;
-		assert(buffer.bounds());
-		assert(buffer.write(set_second));
-		assert(buffer.bounds());
-
-		ConstRawBuffer const_buffer;
-		const_buffer = buffer2;
-
-		const_buffer.reset();
 		assert(const_buffer.bounds());
-		assert(const_buffer.read(copy));
-		assert(const_buffer.bounds());
-		assert(copy == set_first);
 
+		copy = DataSet();
 		ConstRawBuffer const_buffer2;
-		const_buffer2 = const_buffer;
-
+		const_buffer2 = buffer2;
+		
+		assert(const_buffer2.rewind());
 		assert(const_buffer2.bounds());
 		assert(const_buffer2.read(copy));
-		assert(const_buffer2.bounds());
+		assert(copy == set_first);
+		assert(const_buffer2.read(copy));
 		assert(copy == set_second);
-	}
-
-	static void test_structs() {
-		DataSet input_false(0x00, 0x00, 0x00, 0x00);
-		DataSet input_true(~0, ~0, ~0, ~0);
-		DataSet input_inc(0x01, 0x02, 0x03, 0x04);
-		DataSet input_dec(~0 - 1, ~0 - 2, ~0 - 3, ~0 - 4);
-
-		test_struct_rw(input_false);
-		test_struct_rw(input_true);
-		test_struct_rw(input_inc);
-		test_struct_rw(input_dec);
-
-		test_struct_assign(input_false);
-		test_struct_assign(input_true);
-		test_struct_assign(input_inc);
-		test_struct_assign(input_dec);
-
-		test_fields_rw(input_false);
-		test_fields_rw(input_true);
-		test_fields_rw(input_inc);
-		test_fields_rw(input_dec);
-
-		test_fields_assign(input_false);
-		test_fields_assign(input_true);
-		test_fields_assign(input_inc);
-		test_fields_assign(input_dec);
+		assert(const_buffer2.bounds());
 	}
 
 public:
 
 	static void test() {
-		test_structs();
-		test_smash();
-		test_skip();
-		test_raii_copy_ctr();
-		test_raii_copy_opt();
+		test_read_write_assign();
+		test_bounds();
+		test_skip_rewind();
+		test_raii();
+		//test_array(); TODO:
 	}
 
 };
